409e79f7668069137a09f632c6d16a4706cbcaa4
ABsnippet <bandeamit610@gmail.com>
2025-02-23 02:03:25 +0000

    basics of js config

---
+++ 01_basics/01_test.js
@@ -0,0 +1,12 @@
+const accNo = "123456";
+let accname = "Amit";
+var name = "Rahul";
+let accstate;
+acccit = "bgm";
+
+
+
+console.log(accNo);
+
+ console.table([accNo , accname , name , acccit, accNo]);
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let name = "Amit"
+let age = 18
+
+let m = null;
+
+let asd;
+
+console.log(typeof age);
