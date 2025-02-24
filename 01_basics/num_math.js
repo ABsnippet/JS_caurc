0842f8910a3fe5ec9cb74eadd50b5c365d4168e2
ABsnippet <bandeamit610@gmail.com>
2025-02-24 17:39:14 +0000

    math

---
+++ 01_basics/num_math.js
@@ -0,0 +1,27 @@
+const num = 23.768
+// console.log(num);
+
+// const num1 = new Number(400)
+// console.log(num1);
+
+// console.log(num.toPrecision(3))
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// Maths
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.37));
+// console.log(Math.floor(4.95));
+
+// console.log(Math.min(4,6,5));
+
+console.log(Math.floor(Math.random()*6)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()* (max-min +1))+min)
