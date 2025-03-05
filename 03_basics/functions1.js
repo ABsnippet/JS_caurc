7df9d03649f6e4a3deb0f0b6d20276df4d3520b4
ABsnippet <bandeamit610@gmail.com>
2025-03-05 12:51:58 +0000

    funcs2

--- 03_basics/functions1.js
+++ 03_basics/functions1.js
@@ -4,7 +4,7 @@ function addtwonums(num1,num2) {
 
 const result = addtwonums(3,5)
 
-console.log(result);
+// console.log(result);
 
 function loggedin(user = "Sam") {
     if (user === undefined) {
@@ -16,4 +16,36 @@ function loggedin(user = "Sam") {
 
 const name=loggedin()
 
-console.log(name)
+// console.log(name)
+
+function calculatePrice(n1,n2,...numb1){
+    return numb1
+}
+
+// console.log(calculatePrice(20,40,60,80,100))
+
+const user = {
+username : "Amit",
+price : 699
+
+}
+
+
+function handleObject(anyobj){
+    console.log(`Username is ${anyobj.username} and price is ${anyobj.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username : "amit",
+    price:500
+})
+
+const newarray = [1,2,3,45,66]
+
+function arraypass(arr){
+    return arr[2]
+}
+
+console.log(arraypass(newarray));
