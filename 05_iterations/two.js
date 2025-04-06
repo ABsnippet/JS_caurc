5166254694cfaf19c3c6e4708ba493ccaa93e23f
ABsnippet <bandeamit610@gmail.com>
2025-04-06 13:10:37 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let index= 0 
+while (index<=10) {
+    console.log(`The value of index is ${index}`)
+    index=index+2
+}
+
+let myarr = ["amul","abhi","rohit"]
+
+let arr=0
+
+while (arr<myarr.length) {
+    console.log(`the anmes are ${myarr[arr]}`)
+    arr++
+    
+}
+
+let score = 1
+
+do {
+    console.log(`the score is ${score}`)
+    score+=1
+} while (score<=10);
