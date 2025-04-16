0c5b81cc0bbe25b3d4fc6bc2a5c4a9a5ff8fabe3
ABsnippet <bandeamit610@gmail.com>
2025-04-16 13:09:44 +0000

    higher_orderloops

---
+++ 05_iterations/three.js
@@ -0,0 +1,24 @@
+let arr = [1,2,3,4,5,6,7,8,9,0]
+
+// for (const ele of arr) {
+//     console.log(ele)
+// }
+
+// let str = "My Name is AMit"
+
+// for (const element of str) {
+//     console.log(element);
+    
+// }
+
+//Maps
+
+const map = new Map()
+map.set('in','91')
+map.set('fr','92')
+map.set('uk','91')
+map.set('uk','91')
+
+console.log(map);
+
+
